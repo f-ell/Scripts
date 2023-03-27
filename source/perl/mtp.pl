8d8f7bd852f065995ae2ad8110be403a2a0f6d5d
Nico Pareigis <nico@pareigis.de>
2023-03-27 12:54:11 +0200

    feat: dependency checks for mvn executable and unified error handling / exit codes

--- source/perl/mtp.pl
+++ source/perl/mtp.pl
@@ -8,86 +8,97 @@ $" = ', ';
 # TODO:
 # * add documentation
 # * option for uncoloured output
+# * option for dump output, i.e. wait for mvn test to finish
 # * option for json output
-# * change `die` to err calls
 
 my $author    = 'Nico Pareigis';
 my ($program) = $0 =~ m{^.*/(.+)$};
-my $version   = 0.0.1;
+my $version   = 0.0.2;
 
-my sub err {
+my sub err($$) { # exit_code, message
   printf STDERR "$program: %s\n", $_[1];
   exit($_[0]) if $_[0] > 0;
 }
-my sub mvn_err {
-  printf STDERR "[%s] %s\n", colored('ERR', 'ERR'), $_[1];
+
+my sub mvn($$$) { # exit_code, severity, message
+  my $sev = uc $_[1];
+  printf STDOUT "[%s] %s\n", colored($sev, $sev), $_[2];
   exit($_[0]) if $_[0] > 0;
 }
 
-
-my @missing_deps = ();
-
-eval 'use Cwd qw(chdir)';
-push @missing_deps, 'Cwd' if $@;
-eval 'use Term::ANSIColor 4.00 qw(color colored coloralias)';
-push @missing_deps, 'Term::ANSIColor' if $@;
-
-err 2, "dependency not met - @missing_deps" if @missing_deps;
-
-Term::ANSIColor::coloralias('INF', 'bold blue');
-Term::ANSIColor::coloralias('ERR', 'bold red');
-
-my $cwd = Cwd::getcwd();
-my $root = 0;
-
-
-my sub find_mvn_root_rec($) {
-  local $_ = shift;
-  mvn_err 2, 'Fatal: No maven root found' if m{^/$};
-
-  opendir DH, $_ or err 1, 'failed to open dirhandle \''.$_.'\'';
-    my @files = readdir DH;
-  closedir DH or err 1, 'failed to close dirhandle';
-
-  return if grep /^pom\.xml$/, @files;
-  $cwd = Cwd::abs_path($cwd.'/..');
-  __SUB__->($cwd);
+my sub mvn_avail {
+  my $exec = 0;
+  foreach (split ':', $ENV{PATH}) {
+    chop if m{/$};
+    if (not -d $_.'/mvn' and -x _) { $exec = 1; last; }
+  }
+  err 1, 'mvn executable not found in $PATH' unless $exec;
 }
 
-my sub find_mvn_root($) {
-  print '[', colored('INF', 'INF'), '] Looking for maven root directory... ', "\n";
-  find_mvn_root_rec(shift);
-  print '[', colored('INF', 'INF'), '] Found maven root at ', $cwd, "\n";
+my sub mod_avail {
+  my @missing_deps = ();
+
+  eval 'use Cwd qw(chdir)';
+  push @missing_deps, 'Cwd' if $@;
+  eval 'use Term::ANSIColor 4.00 qw(color colored coloralias)';
+  push @missing_deps, 'Term::ANSIColor' if $@;
+
+  err 1, "dependency not met - @missing_deps" if @missing_deps;
+}
+
+my sub dep_check {
+  mvn_avail();
+  mod_avail();
+  Term::ANSIColor::coloralias('INF', 'bold blue');
+  Term::ANSIColor::coloralias('ERR', 'bold red');
+}
+
+my sub find_mvn_root_rec($) { local $_ = shift;
+  mvn 2, 'ERR', 'Fatal: No maven root found' if m{^/$};
+
+  opendir DH, $_ or err 1, 'failed to open dirhandle \''.$_.'\'';
+    return $_ if grep /^pom\.xml$/, readdir DH;
+  closedir DH or err 1, 'failed to close dirhandle \''.$_.'\'';
+
+  $_ = Cwd::abs_path($_.'/..');
+  __SUB__->($_);
+}
+
+my sub find_mvn_root {
+  my ($cwd, $root) = (Cwd::getcwd(), 0);
+  mvn 0, 'INF', 'Looking for maven root directory...';
+  $cwd = find_mvn_root_rec($cwd);
+  mvn 0, 'INF', 'Found maven root at '.$cwd;
   chdir $cwd;
 }
 
-find_mvn_root($cwd);
+# preliminary checks
+dep_check();
+find_mvn_root();
 
-
+# parse test output
 my ($PACK, $FILE) = ('')x2;
 my ($tt, $tf, $te, $ts) = (0)x4;
 
-# FIX: test maven installed / available in path
-open FH, '-|', 'mvn test 2>/dev/null' or die $!;
+open FH, '-|', 'mvn test 2>/dev/null' or err 1, 'failed to spawn \'mvn test\'';
 
 while (<FH>) {
-  next if not /^\[\w+\]/;
+  next unless /^\[\w+\]/;
 
   if (/^\[INFO\] Running ([\w.]+)\.(\w+)$/) {
     if ($1 ne $PACK) {
       # print "\n" if $PACK;
       $PACK = $1;
-      print '[', colored('INF', 'INF'), '] Testing package ', $PACK, "\n";
+      mvn 0, 'INF', 'Testing package '.$PACK;
     }
     $FILE = $2;
-    print '[', colored('INF', 'INF'), '] Running ', $FILE, "\n";
+    mvn 0, 'INF', 'Running '.$FILE;
   }
 
   if (/^\[(\w+)\] Tests run: (.*), Time elapsed:/) {
-    my $sev = substr($1, 0, 3);
+    mvn 0, substr($1, 0, 3), $FILE.' results:';
     $2 =~ /^(\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+)/;
     my ($t, $f, $e, $s) = ($1, $2, $3, $4);
-    print '[', colored($sev, $sev), '] ', $FILE, ' results:', "\n";
     print '  Ran     : ', $t, "\n";
     print '  Passed  : ', ($t - $e), "\n";
     print '  Failed  : ', $f, "\n";
@@ -97,7 +108,7 @@ while (<FH>) {
 
   # FIX: push each test to array and eval after loop + include tally
   if (/^\[ERROR\] Errors:/) {
-    print '[', colored('ERR', 'ERR'), '] Failed tests:', "\n";
+    mvn 0, 'ERR', 'Failed tests:';
     until (not local $_ = <FH>) {
       last if /^\[INFO\]/;
       /^\[ERROR\]\s+(\w+)\.(\w+)/;
@@ -107,15 +118,13 @@ while (<FH>) {
 
   if (/^\[INFO\] BUILD (\w+)$/) {
     my $sev = $1 =~ /^SUCCESS$/ ? 'INF' : 'ERR';
-    print '[', colored($sev, $sev), '] Build '.lc $1, "\n";
+    mvn 0, $sev, 'Build '.lc $1;
   }
 
   if (/^\[INFO\] Total time:\s+(.*)$/) {
-    print '[', colored('INF', 'INF'), '] Took '.$1, "\n";
+    mvn 0, 'INF', 'Took '.$1;
   }
 }
 
-# FIX:
-# * don't die when maven has non-zero exit status
-# * or "mvn_err $?, maven fatal"
-close FH or die $!;
+close FH or $? = $? == 256 ? 1 : $?;
+$? > 0 and mvn $?, 'ERR', 'Fatal: maven returned non-zero exit status';
