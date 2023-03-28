52da66204beda1d134eafcee05d6af46aaad7463
Nico Pareigis <nico@pareigis.de>
2023-03-28 22:24:16 +0200

    feat: preliminary support for json output

--- source/perl/mtp.pl
+++ source/perl/mtp.pl
@@ -5,8 +5,6 @@ $" = ', ';
 
 # TODO:
 # * options
-#   * json output
-#   * dump output (i.e. wait for mvn to finish)
 #   * omit failed test summary
 #   * omit / only tally
 #   * only test results
@@ -14,9 +12,9 @@ $" = ', ';
 
 my $author    = 'Nico Pareigis';
 my ($program) = $0 =~ m{^.*/(.+)$};
-my $version   = '0.0.3';
+my $version   = '0.0.4';
 
-my %OPTS = ( colour => 1 );
+my %OPTS = ( colour => 1, json => 0, json_pretty => 0 );
 
 my sub err($$) { # exit_code, message
   printf STDERR "$program: %s\n", $_[1];
@@ -47,8 +45,14 @@ my sub help {
       -h | --help
           Print help information and exit.
 
+      -j | --json
+          Output test data in JSON format, requires JSON::PP to be installed.
+
       -n | --no-colour
           Disable coloured output, makes Term::ANSIColor an optional dependency.
+
+      -p | --prettify-json
+          Output JSON data in prettified format, implies -j.
 
       -v | --version
           Print version information and exit.
@@ -64,6 +68,7 @@ my sub help {
 
       Modules:
       Cwd
+      JSON::PP        (required with -j, -p)
       Term::ANSIColor (optional with -n)
 
   VERSION
@@ -93,6 +98,7 @@ my sub mod_avail {
   my @missing = ();
   my @mods = (
     { mod => 'Cwd', args => 'qw(chdir)' },
+    { mod => 'JSON::PP', cond => $OPTS{json} },
     { mod => 'Term::ANSIColor',
       cond => $OPTS{colour},
       args => '4.00 qw(color colored coloralias)',
@@ -126,11 +132,123 @@ my sub find_mvn_root_rec($) { local $_ = shift;
 }
 
 my sub find_mvn_root {
+  # FIX: suppress output, and push $cwd to %JSON with -j
   my ($cwd, $root) = (Cwd::getcwd(), 0);
   mvn 0, 'INF', 'Looking for maven root directory...';
   $cwd = find_mvn_root_rec($cwd);
   mvn 0, 'INF', 'Found maven root at '.$cwd;
   chdir $cwd;
+}
+
+my sub parse_json($) {
+  my %JSON = ();
+  my ($FH, $PACKAGE, $CLASS) = (shift, ('')x2);
+  my (@tally, @failed) = ()x2;
+
+  # synchronous parse
+  while (<$FH>) {
+    next unless /^\[\w+\]/;
+
+    if (/^\[INFO\] Running ([\w.]+)\.(\w+)$/) {
+      if ($1 ne $PACKAGE) { $PACKAGE = $1; $JSON{testResults}{$PACKAGE} = {}; }
+      $CLASS = $2;
+    }
+
+    if (/^\[\w+\] Tests run: (.*), Time elapsed:/) {
+      $1 =~ /^(\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+)/;
+      $JSON{testResults}{$PACKAGE}{$CLASS} = {
+        test => $1, fail => $2, error => $3, skip => $4
+      };
+      my $i = 0;
+      $tally[$i++] += $_ foreach ($1, $2, $3, $4);
+      undef $i;
+    }
+
+    if (/^\[ERROR\] Errors:/) {
+      while (local $_ = <$FH>) {
+        last if /^\[INFO\]/;
+        /^\[ERROR\]\s+(\w+)\.(\w+)/;
+        push @{ $JSON{maven}{failedTests}{$1} }, $2;
+      }
+    }
+
+    if (/^\[INFO\] BUILD (\w+)$/) { $JSON{maven}{buildStatus} = $1; }
+    if (/^\[INFO\] Total time:\s+(.*)$/) { $JSON{maven}{totalTime} = $1 }
+  }
+
+  # output
+  print JSON::PP->new->pretty($OPTS{json_pretty})->encode(\%JSON);
+}
+
+my sub parse_plain($) {
+  my ($PACKAGE, $CLASS, $TIME, $BUILD) = ('')x4;
+  my (@tally, @failed) = ()x2;
+
+  # synchronous parse
+  my $FH = shift;
+  while (<$FH>) {
+    next unless /^\[\w+\]/;
+
+    if (/^\[INFO\] Running ([\w.]+)\.(\w+)$/) {
+      if ($1 ne $PACKAGE) {
+        $PACKAGE = $1; mvn 0, 'INF', 'Testing package '.$PACKAGE;
+      }
+      $CLASS = $2; mvn 0, 'INF', 'Running '.$CLASS;
+    }
+
+    if (/^\[(\w+)\] Tests run: (.*), Time elapsed:/) {
+      mvn 0, substr($1, 0, 3), $CLASS.' results:';
+      $2 =~ /^(\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+)/;
+      my ($t, $f, $e, $s) = ($1, $2, $3, $4);
+
+      my $i = 0;
+      $tally[$i++] += $_ foreach ($t, $f, $e, $s);
+      undef $i;
+
+      print '  Ran     : ', $t, "\n";
+      print '  Passed  : ', ($t - $f - $e), "\n";
+      print '  Failed  : ', $f, "\n";
+      print '  Errored : ', $e, "\n";
+      print '  Skipped : ', $s, "\n";
+    }
+
+    if (/^\[ERROR\] Errors:/) {
+      while (local $_ = <$FH>) {
+        last if /^\[INFO\]/;
+        /^\[ERROR\]\s+(\w+)\.(\w+)/;
+        push @failed, [$1, $2];
+      }
+    }
+
+    if (/^\[INFO\] BUILD (\w+)$/) { $BUILD = $1; }
+    if (/^\[INFO\] Total time:\s+(.*)$/) { $TIME = $1; }
+  }
+
+  # test summary
+  mvn 0, 'INF', 'Summary';
+  mvn 0, $tally[1] + $tally[2] == 0 ? 'INF' : 'ERR', 'Test summary:';
+  print '  Ran     : ', $tally[0], "\n";
+  print '  Passed  : ', $tally[0] - $tally[1] - $tally[2], "\n";
+  print '  Failed  : ', $tally[1], "\n";
+  print '  Errored : ', $tally[2], "\n";
+  print '  Skipped : ', $tally[3], "\n";
+
+  if (@failed) {
+    mvn 0, 'ERR', 'Failed tests:';
+    print '  ', $_->[0], ' -> ', $_->[1], "\n" foreach @failed;
+  }
+
+  my $sev = $BUILD =~ /^SUCCESS$/ ? 'INF' : 'ERR';
+  mvn 0, $sev, 'Build '.lc $BUILD;
+  mvn 0, 'INF', 'Time taken: '.$TIME;
+}
+
+my sub parse($) {
+  if ($OPTS{json}) {
+    parse_json(shift);
+  } else {
+    parse_plain(shift);
+  }
 }
 
 # option processing
@@ -140,8 +258,14 @@ while (local $_ = shift) {
   if (/^-h|--help$/) {
     help();
   }
+  elsif (/^-j|--json$/) {
+    $OPTS{json} = 1;
+  }
   elsif (/^-n|--no-colour$/) {
     $OPTS{colour} = 0;
+  }
+  elsif (/^-p|--prettify-json$/) {
+    $OPTS{json} = 1; $OPTS{json_pretty} = 1;
   }
   elsif (/^-v|--version$/) {
     version();
@@ -156,68 +280,8 @@ dep_check();
 find_mvn_root();
 
 # parse test output
-my ($PACKAGE, $CLASS, $TIME, $BUILD) = ('')x4;
-my (@tally, @failed) = ()x2;
-
-open FH, '-|', 'mvn test 2>/dev/null' or err 1, 'failed to spawn \'mvn test\'';
-
-while (<FH>) {
-  next unless /^\[\w+\]/;
-
-  if (/^\[INFO\] Running ([\w.]+)\.(\w+)$/) {
-    if ($1 ne $PACKAGE) {
-      $PACKAGE = $1;
-      mvn 0, 'INF', 'Testing package '.$PACKAGE;
-    }
-    $CLASS = $2;
-    mvn 0, 'INF', 'Running '.$CLASS;
-  }
-
-  if (/^\[(\w+)\] Tests run: (.*), Time elapsed:/) {
-    mvn 0, substr($1, 0, 3), $CLASS.' results:';
-    $2 =~ /^(\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+)/;
-    my ($t, $f, $e, $s) = ($1, $2, $3, $4);
-
-    my $i = 0;
-    $tally[$i++] += $_ foreach ($t, $f, $e, $s);
-    undef $i;
-
-    print '  Ran     : ', $t, "\n";
-    print '  Passed  : ', ($t - $e), "\n";
-    print '  Failed  : ', $f, "\n";
-    print '  Errored : ', $e, "\n";
-    print '  Skipped : ', $s, "\n";
-  }
-
-  if (/^\[ERROR\] Errors:/) {
-    while (local $_ = <FH>) {
-      last if /^\[INFO\]/;
-      /^\[ERROR\]\s+(\w+)\.(\w+)/;
-      push @failed, [$1, $2];
-    }
-  }
-
-  if (/^\[INFO\] BUILD (\w+)$/) { $BUILD = $1; }
-  if (/^\[INFO\] Total time:\s+(.*)$/) { $TIME = $1; }
-}
-
-# test summary
-mvn 0, 'INF', 'Summary';
-mvn 0, $tally[1] + $tally[2] == 0 ? 'INF' : 'ERR', 'Test summary:';
-print '  Ran     : ', $tally[0], "\n";
-print '  Passed  : ', $tally[0] - $tally[1] - $tally[2], "\n";
-print '  Failed  : ', $tally[1], "\n";
-print '  Errored : ', $tally[2], "\n";
-print '  Skipped : ', $tally[3], "\n";
-
-if (@failed) {
-  mvn 0, 'ERR', 'Failed tests:';
-  print '  ', $_->[0], ' -> ', $_->[1], "\n" foreach @failed;
-}
-
-my $sev = $BUILD =~ /^SUCCESS$/ ? 'INF' : 'ERR';
-mvn 0, $sev, 'Build '.lc $BUILD;
-mvn 0, 'INF', 'Time taken: '.$TIME;
-
-close FH or $? = $? == 256 ? 1 : $?;
+open my $FH, '-|', 'mvn test 2>/dev/null' or err 1, 'failed to spawn \'mvn test\'';
+  parse($FH);
+close $FH or $? = $? == 256 ? 1 : $?;
+# FIX: suppress output and push $? to %JSON with -j
 $? > 0 and mvn $?, 'ERR', 'Fatal: maven returned non-zero exit status';
